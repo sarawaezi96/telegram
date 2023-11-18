65f978a5b1c3d74943f26b9cd82029a3719bd0ab
sarawaezi96 <fvaezi076@gmail.com>
2023-11-18 15:40:00 +0330

    init

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class chates extends StatelessWidget {
+  const chates({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
